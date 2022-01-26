4f56559890d07a1f0dcf85a1b93921b632279b26
Frederic_Bui <bui.frederic.pro@gmail.com>
2022-01-26 13:07:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function displayNone() {
+  // select div with id container
+  var element = document.querySelector("#container");
+  // Apply css display none to div id container
+  element.classList.toggle("displayNone");
+}
